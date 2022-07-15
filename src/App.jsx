343c757635eabc9193c0fac7af436a2a8e177081
Yousef Ayman <106432204+Yussif20@users.noreply.github.com>
2022-07-15 19:06:56 +0200

    adding title and a paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,29 @@ function App() {
           <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
       </AppBar>
-      <main></main>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              hello everyone I'm trying to make this paragraph as long as
+              possiable to see if this site worth all that reputation
+            </Typography>
+          </Container>
+        </div>
+      </main>
     </>
   );
 }
